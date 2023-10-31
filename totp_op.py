234dc7b1fcab294e759f55fc51aff18766108fec
realiron00 <ic1595@naver.com>
2023-10-31 17:32:41 +0900

    10311732

---
+++ totp_op.py
@@ -0,0 +1,13 @@
+import pyotp
+import time
+import base64
+
+otp_key = base64.b32encode("test".encode())
+
+now = int(time.time())
+totp = pyotp.TOTP(otp_key, digits=3, interval=180)
+otp1 = totp.at(now-180) # before 180s
+otp2 = totp.at(now)
+otp3 = totp.at(now+180) # after 180s
+print(otp2) # OTP Password as of Current Time
+print(totp.verify(otp2)) # True
