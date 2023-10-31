a6b11c83f35436c6473f5da808f8acc209b5b51e
realiron00 <ic1595@naver.com>
2023-10-31 22:42:57 +0900

    10312242

--- totp.py
+++ totp.py
@@ -1,1 +1,54 @@
 import time
+import base64
+import hmac
+import hashlib
+import struct
+
+#===============================================================================
+# TOTP (Time-based One-time Password) Algorithm
+#
+# key: shared secret key
+# time: time in seconds since the Epoch
+# digits: number of digits in the OTP
+# interval: time interval in seconds for OTP validity
+#
+# return: OTP as string
+#===============================================================================
+def totp(key, time, digits, interval):
+    counter = time // interval
+    return hotp(key, counter, digits)
+
+#===============================================================================
+# HOTP (HMAC-based One-time Password) Algorithm
+#
+# key: shared secret key
+# counter: counter value
+# digits: number of digits in the OTP
+#
+# return: OTP as string
+#===============================================================================
+def hotp(key, counter, digits):
+    # Convert the counter to bytes
+    counter_bytes = struct.pack(">Q", counter)
+
+    # Decode the secret key
+    key = base64.b32decode(key)
+
+    # Compute the HMAC-SHA-1 hash
+    hmac_digest = hmac.new(key, counter_bytes, hashlib.sha1).digest()
+
+    # Dynamic Truncation
+    offset = hmac_digest[-1] & 0x0F
+    hash = hmac_digest[offset:offset+4]
+    binary_code = struct.unpack(">I", hash)[0] & 0x7FFFFFFF
+
+    # Generate an OTP of the specified length
+    otp = str(binary_code % 10 ** digits)
+    return otp.zfill(digits)
+
+if __name__ == "__main__":
+    otp_key = base64.b32encode("test".encode())
+    now = int(time.time())
+    otp1 = totp(otp_key, now, digits=10, interval=180)
+
+    print(otp1) # OTP Password as of Current Time
--- totp_op.py
+++ totp_op.py
@@ -5,7 +5,7 @@ import base64
 otp_key = base64.b32encode("test".encode())
 
 now = int(time.time())
-totp = pyotp.TOTP(otp_key, digits=3, interval=180)
+totp = pyotp.TOTP(otp_key, digits=10, interval=180)
 otp1 = totp.at(now-180) # before 180s
 otp2 = totp.at(now)
 otp3 = totp.at(now+180) # after 180s
